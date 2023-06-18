c67a878d046f946abf9cf1cd8da9240e5abb99ee
Igor_Sidorov <27107390q@gmail.com>
2023-06-18 23:33:31 +0300

    Demoqa

--- tests/test_check_icon_demoqa.py
+++ tests/test_check_icon_demoqa.py
@@ -4,17 +4,11 @@ from pages.demoqa import DemoQa
 def test_icon_exists(browser):
     demo_qa_page = DemoQa(browser)
     demo_qa_page.visit()
-    assert demo_qa_page.equal_url()
     assert demo_qa_page.exist_icon()
 
-
-
-    # browser.get('https://demoqa.com/')
-    # icon = browser.find_element(By.CSS_SELECTOR, '#app > header > a')
-    #
     # if icon is None:
-    #     print("Элемент не найден")
+    #   print("Элемент не найден")
     # else:
-    #     print("Элемент найден")
-    #
+    #  print("Элемент найден")
+
 
